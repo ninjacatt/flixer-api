f82a33b0d8a4f672e60beb337b18d4038565eeec
Minh <minhdevspace@gmail.com>
2017-05-19 14:01:57 -0700

    initial setup for project

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+
+const app = express();
+const index = require('./routes/index');
+const movies = require('./routes/movies');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+
+app.use('/', index);
+//app.use('/movies', movies);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error middleware
+app.use((err, req, res, next) => {
+  // render the error page
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+app.listen(3000, () => {
+  console.log('Flixer listening on port 3000!');
+});
+
